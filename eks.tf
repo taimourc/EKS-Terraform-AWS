d3aad5ad93dab101dd24a0e3b11b56d8d54c30e8
taimourc <taimourchaudhry@gmail.com>
2025-09-07 01:15:07 -0400

    Add name

--- eks.tf
+++ eks.tf
@@ -1,4 +1,4 @@
-resource "aws_eks_cluster" "" {
+resource "aws_eks_cluster" "eks_cluster" {
   name     = var.cluster_name
   role_arn = aws_iam_role.eks_cluster.arn
   version  = var.kubernetes_version
