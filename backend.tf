66990b76c3c75b40061c33266bc9ea2521a5075b
taimourc <taimourchaudhry@gmail.com>
2025-09-07 01:09:12 -0400

    Backend change

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "aws-eks-terraform-state-1"
+    bucket         = "aws-eks-terraform-state-2"
     key            = "eks/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
